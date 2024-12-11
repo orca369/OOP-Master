196f13bbad0f97f8d4e4672c3a5c543483cb3880
Abdelaziz shahin <abd91business@gmail.com>
2024-12-11 15:48:57 +0100

    creating Meal order

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
 //        coke.printItem();
 //        Item avocado = new Item("Topping","avocado",1.50);
 //        avocado.printItem();
-    Burger burger = new Burger("regular",4.00);
-    burger.addToppings("BACON","STRING","MAYO");
-    burger.printItem();
+//    Burger burger = new Burger("regular",4.00);
+//    burger.addToppings("BACON","STRING","MAYO");
+//    burger.printItem();
     }
 }
