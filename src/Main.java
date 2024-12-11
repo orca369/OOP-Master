bafba741a5bd59ba7385d6e0751f2c7a2964db23
Abdelaziz shahin <abd91business@gmail.com>
2024-12-11 15:31:43 +0100

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("dring","coke",1.50);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
-        Item avocado = new Item("Topping","avocado",1.50);
-        avocado.printItem();
+//        Item coke = new Item("dring","coke",1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//        Item avocado = new Item("Topping","avocado",1.50);
+//        avocado.printItem();
+    Burger burger = new Burger("regular",4.00);
+    burger.addToppings("BACON","STRING","MAYO");
+    burger.printItem();
     }
 }
